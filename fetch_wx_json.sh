3a3f5b1605235784d56a5f8dbfd508c0ac06f816
Nick Ryberg <nryberg@gmail.com>
2023-04-11 21:27:59 -0500

    Millerize

---
+++ fetch_wx_json.sh
@@ -0,0 +1,3 @@
+!#/bin/bash
+
+curl  https://www.aviationweather.gov/cgi-bin/json/MetarJSON.php?bbox=-93.271681,44.839788,-93.141143,44.920127 > ./wx.json
---
+++ miller_process.sh
@@ -0,0 +1,1 @@
+mlr --ijson --omd flatten then cut -r -f '^features.2' ./wx.json
