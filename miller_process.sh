52ed3b30e7a85e0d3e8a84bebdaed1a48668b06c
Nick Ryberg <nryberg@gmail.com>
2023-04-14 06:31:30 -0500

    Working the codes

--- miller_process.sh
+++ miller_process.sh
@@ -1,1 +1,3 @@
-mlr --ijson --omd flatten then cut -r -f '^features.2.properties' ./wx.json | sed  's/features\.2\.properties\.//g'
+# mlr --ijson --omd flatten then cut -r -f '^features.2.properties' ./wx.json | sed  's/features\.2\.properties\.//g'
+mlr --c2m rename -g -r '^features.2.properties.,' ./wx.csv
+#mlr --c2p cat ./wx.csv
