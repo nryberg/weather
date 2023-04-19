c8e8d15d80e75b61b088c8fa58c73d579b8d8f21
Nick Ryberg <nryberg@gmail.com>
2023-04-19 06:42:58 -0500

    Update duck_daily_stat.sql

--- duck_daily_stat.sql
+++ duck_daily_stat.sql
@@ -1,13 +1,24 @@
 with fix_date as (
     SELECT
-    select
-        substr(observed_timestamp_s, 1, 10) as obs_date
+        substr(observed_timestamp_s, 1, 10) as obs_date,
+        temp_celsius
     from
         wx_load
-    limit
-        10
+),
+daily_max as (
+    SELECT
+        obs_date,
+        max(temp_celsius) as max_temp_celsius
+    FROm
+        fix_date
+    GROUP BY
+        obs_date
 )
 SELECT
-    *
-FROm
-    fix_date
+    obs_date,
+    max_temp_celsius,
+    round((max_temp_celsius * 9 / 5)) + 32 as max_temp_fahrenheit
+from
+    daily_max
+LIMIT
+    10
