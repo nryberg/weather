6d629a2de491f04e52018a389812379540388d3d
Nick Ryberg <nryberg@gmail.com>
2023-04-25 11:04:00 -0500

    Sampler Script

--- build_sample.sh
+++ build_sample.sh
@@ -1,1 +1,1 @@
-mlr --c2p cut -f Observed_Date,Temp_Celsius,Wind_Speed,Wind_Direction,Cover,Visibility then head ./weather_log.csv
+mlr --csv cut -f Observed_Date,Temp_Celsius,Wind_Speed,Wind_Direction,Cover,Visibility ./weather_log.csv > sample_wx.csv
