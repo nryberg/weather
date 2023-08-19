8528f7d33eae27a77e481152b67204308bbcf737
nryberg <nryberg@gmail.com>
2023-08-18 21:42:04 -0500

    Add Minio push

--- fetch_wx_json.sh
+++ fetch_wx_json.sh
@@ -1,1 +1,3 @@
-curl  https://www.aviationweather.gov/cgi-bin/json/MetarJSON.php?bbox=-93.271681,44.839788,-93.141143,44.920127 > "./wx.$(date +%Y-%m-%d_%H:%M).json"
+curl  https://www.aviationweather.gov/cgi-bin/json/MetarJSON.php?bbox=-93.271681,44.839788,-93.141143,44.920127 > "/home/nick/Develop/GitHub/nryberg/weather/wx.$(date +%Y-%m-%d_%H:%M).json"
+
+s3cmd put "/home/nick/Develop/GitHub/nryberg/weather/wx.$(date +%Y-%m-%d_%H:%M).json" s3://weather
