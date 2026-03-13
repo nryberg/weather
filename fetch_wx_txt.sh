82056286abedeb70a2883b916caafb5657fb8fbc
nryberg <nryberg@gmail.com>
2026-03-13 16:57:24 -0500

    update api endpoint

--- fetch_wx_txt.sh
+++ fetch_wx_txt.sh
@@ -1,3 +1,3 @@
-curl "https://aviationweather.gov/cgi-bin/data/metar.php?ids=KMSP&hours=2&order=id%2C-obs&sep=true" -o "/home/nick/Develop/GitHub/nryberg/weather/data/wx.$(date +%Y-%m-%d_%H:%M).txt"
+curl "https://aviationweather.gov/api/data/metar?ids=KMSP&format=json&hours=2&sep=true"-o "/home/nick/Develop/GitHub/nryberg/weather/data/wx.$(date +%Y-%m-%d_%H:%M).json"
 
 s3cmd put "/home/nick/Develop/GitHub/nryberg/weather/data/wx.$(date +%Y-%m-%d_%H:%M).txt" s3://weather
