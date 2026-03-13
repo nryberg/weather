8b19effcf1703e6ea0e9f3d0c9169e4000d4266e
nryberg <nryberg@gmail.com>
2026-03-13 17:05:03 -0500

    Fix typos

--- fetch_wx_txt.sh
+++ fetch_wx_txt.sh
@@ -1,3 +1,3 @@
-curl "https://aviationweather.gov/api/data/metar?ids=KMSP&format=json&hours=2&sep=true"-o "/home/nick/Develop/GitHub/nryberg/weather/data/wx.$(date +%Y-%m-%d_%H:%M).json"
+curl "https://aviationweather.gov/api/data/metar?ids=KMSP&format=json&hours=2&sep=true" -o "/home/nick/Develop/GitHub/nryberg/weather/data/wx.$(date +%Y-%m-%d_%H:%M).json"
 
-s3cmd put "/home/nick/Develop/GitHub/nryberg/weather/data/wx.$(date +%Y-%m-%d_%H:%M).txt" s3://weather
+s3cmd put "/home/nick/Develop/GitHub/nryberg/weather/data/wx.$(date +%Y-%m-%d_%H:%M).json" s3://weather
