974f5b36903c20fb9aaf41d4bc7d34498a4adaad
nryberg <nryberg@gmail.com>
2026-03-22 14:57:41 -0500

    Clean up

--- working.fetch_wx_json.sh
+++
@@ -1,3 +0,0 @@
-curl  "https://www.aviationweather.gov/cgi-bin/json/MetarJSON.php?bbox=-93.271681,44.839788,-93.141143,44.920127" -o "/home/nick/Develop/GitHub/nryberg/weather/wx.$(date +%Y-%m-%d_%H:%M).json"
-
-s3cmd put "/home/nick/Develop/GitHub/nryberg/weather/wx.$(date +%Y-%m-%d_%H:%M).json" s3://weather
